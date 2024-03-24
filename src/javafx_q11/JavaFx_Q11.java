a234cf3a2dca3e067575ba7f19aaa4566f430db7
maria <marcavmer@alu.edu.gva.es>
2024-03-24 18:36:45 +0100

    initialization

---
+++ src/javafx_q11/JavaFx_Q11.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafx_q11;
+
+/**
+ *
+ * @author Asus
+ */
+public class JavaFx_Q11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
