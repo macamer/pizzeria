25acbe037e78ad9cd5e0af9f9f41480cba33ce28
maria <marcavmer@alu.edu.gva.es>
2024-03-25 12:39:02 +0100

    add FXMLAlta , FXMLAltaController, fxmlAlta.css

---
+++ src/Vistas/FXMLAltaController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Vistas;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class FXMLAltaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private Button btnIntroPedido;
+
+
+}
